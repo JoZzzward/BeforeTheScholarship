181983098486337e25ff0c9d2de37245d4e22734
Kirill Dyakonov <jozzzwardtm@mail.ru>
2023-02-08 20:24:31 +0300

    Initialize Create method in SettingsFactory.cs

--- Shared/BeforeTheScholarship.Settings/SettingsFactory.cs
+++ Shared/BeforeTheScholarship.Settings/SettingsFactory.cs
@@ -1,5 +1,18 @@
 ﻿namespace BeforeTheScholarship.Settings;
 
-public class SettingsFactory
+using Microsoft.Extensions.Configuration;
+
+public static class SettingsFactory
 {
+    public static IConfiguration Create(IConfiguration? configuration = null)
+    {
+        var conf = configuration ?? new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json", optional: false)
+                                        .AddJsonFile("appsettings.development.json", optional: true)
+                                        .AddEnvironmentVariables()
+                                        .Build();
+
+        return conf;
+    }
 }
