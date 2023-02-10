33450f5522e71cf5013703f66e05804cbc7f5449
Kirill Dyakonov <jozzzwardtm@mail.ru>
2023-02-11 00:25:26 +0300

    Add common 'BaseEntity' class for models in BeforeTheScholarship.Entities

---
+++ Data/BeforeTheScholarship.Entities/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeforeTheScholarship.Entities.Common;
+
+[Index("Uid",IsUnique = true, Name = "Uid_Index")]
+[Obsolete]
+public class BaseEntity
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public Guid Uid { get; set; } = Guid.NewGuid();
+}
